56c987e9757b9515857a22d67a1bbbb764537ed9
Vladimir Romanovsky <vladimir_roman@mail.ru>
2021-12-03 10:53:17 +0500

    TabControl inserted

---
+++ Melts_Base/Melt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melts_Base
+{
+    internal class Melt
+    {
+        string MeltNumber;
+        string MeltName;
+        string AlloyName;
+        string AlloyDescription;
+        string Electrode;
+        string ElectrodeDescription;
+        string MelterNumber;
+        string MelterName;
+        string MelterDescription;
+        string AssignmentNumber;
+        string AssignmentName;
+        string AssignmentDescription;
+        string Directions;
+        string DirectionsDescription;
+        string Contract;
+        string ContractDescription;
+        string Purpose;
+        string PurposeDescription;
+        int    IngotDiameter;
+
+        
+
+         
+        
+
+
+
+
+    }
+}
