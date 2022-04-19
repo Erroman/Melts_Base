21c066b045f312071d8358173552dc202b7a0c7f
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-04-19 16:38:23 +0500

    Oracle model created! With 3 fields in the melts31 table.

---
+++ Melts_Base/OracleModels/Melts31.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Melts_Base.OracleModels
+{
+    public partial class Melts31
+    {
+        public decimal MeltId { get; set; }
+        public string Meltnumber { get; set; }
+        public DateTime Meltdate { get; set; }
+    }
+}
---
+++ Melts_Base/OracleModels/ModelContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Melts_Base.OracleModels
+{
+    public partial class ModelContext : DbContext
+    {
+        public ModelContext()
+        {
+        }
+
+        public ModelContext(DbContextOptions<ModelContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Melts31> Melts31s { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseOracle("User Id=MELTSBASE;Password=meltsbase31SS;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME= pdb1.mshome.net)));");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("MELTSBASE")
+                .UseCollation("USING_NLS_COMP");
+
+            modelBuilder.Entity<Melts31>(entity =>
+            {
+                entity.HasKey(e => e.MeltId)
+                    .HasName("MELTS31_PK");
+
+                entity.ToTable("MELTS31");
+
+                entity.Property(e => e.MeltId)
+                    .HasColumnType("NUMBER")
+                    .HasColumnName("MELT_ID");
+
+                entity.Property(e => e.Meltdate)
+                    .HasColumnType("DATE")
+                    .HasColumnName("MELTDATE");
+
+                entity.Property(e => e.Meltnumber)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("MELTNUMBER");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
