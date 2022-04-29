1c96e1272bcf46bf5d4c36238b45f2adcfdfa603
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-04-29 16:18:47 +0500

    The start and end boundaries of melt's date are bound to the view model.

--- Melts_Base/ViewModel/DateFilter.cs
+++ Melts_Base/ViewModel/DateFilter.cs
@@ -1,16 +1,44 @@
-﻿using System;
+﻿using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Melts_Base.ViewModel
 {
     internal class DateFilter : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        void OnPropertyChanged([CallerMemberName] string propertyName = null) 
+        { 
+            PropertyChanged(this,new PropertyChangedEventArgs(propertyName));
+        }
+            
+
+        string startDate;
+        public string StartDate
+        {
+            get => startDate;
+            set
+            {
+                if (startDate == value) return;
+                startDate = value;
+                OnPropertyChanged();
+            }
+        }
+        string endDate;
+        public string EndDate
+        {
+            get => endDate;
+            set
+            {
+                if (endDate == value) return;
+                endDate = value;
+                OnPropertyChanged();
+
+            }
+        }
     }
 }
