d4a8c4290e51598f9e8eb81478db9b261f0d6a3a
Vladimir Romanovskiy <vladimir_roman@mail.ru>
2022-05-05 23:23:19 +0500

    Added new class for ViewModel of dateGrid1.

---
+++ Melts_Base/ViewModel/ObservableMeltsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Melts_Base.ViewModel
+{
+    internal class ObservableMeltsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
