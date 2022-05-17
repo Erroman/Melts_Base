4e876b8880129e8dd507b9995499c923c7cfdbda
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-05-17 15:51:44 +0500

    Added new Melt.cs class to the Melts_Base project form ConnectionTo Oracle where it was generated from the Oracle table.

---
+++ Melts_Base/OracleModels/Melt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melts_Base
+{
+    public class Melt
+    {
+        public int MeltId { get; set; }
+        public string MeltNumber { get; set; }
+        public DateOnly? MeltDate { get; set; }
+        public string AlloyName { get; set; }
+        public string AlloyIndex { get; set; }
+        public short? MouldSet { get; set; }
+        public short? ElectrodeDiameter { get; set; }
+        public string MelterNumber { get; set; }
+        public string MelterName { get; set; }
+        public string TEKNumber { get; set; }
+        public string IL_UiS_SHN { get; set; }
+        public string Contract { get; set; }  
+        public string Supplement { get; set; }       
+        public string Purpose { get; set; }
+        public short? IngotDiameter { get; set; }
+    }
+ }
