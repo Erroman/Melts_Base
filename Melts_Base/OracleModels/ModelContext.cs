d5ff4f17adacca25c4c16fa23cca0281d8e73768
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-05-17 16:14:43 +0500

    We have models for two Oracle tables in Melts_Base project by copying from ConnectionToOracle ModelContext.cs file.

--- Melts_Base/OracleModels/ModelContext.cs
+++ Melts_Base/OracleModels/ModelContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace Melts_Base.OracleModels
+namespace OracleConnection.OracleModels
 {
     public partial class ModelContext : DbContext
     {
@@ -16,7 +16,7 @@ namespace Melts_Base.OracleModels
         {
         }
 
-        public virtual DbSet<Melts31> Melts31s { get; set; }
+        public virtual DbSet<Melts31> Melts31s { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -31,6 +31,131 @@ namespace Melts_Base.OracleModels
         {
             modelBuilder.HasDefaultSchema("MELTSBASE")
                 .UseCollation("USING_NLS_COMP");
+
+            modelBuilder.Entity<Melt31>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("MELT31");
+
+                entity.Property(e => e.DateClose)
+                    .HasColumnType("DATE")
+                    .HasColumnName("DATE_CLOSE");
+
+                entity.Property(e => e.DateZap)
+                    .HasColumnType("DATE")
+                    .HasColumnName("DATE_ZAP");
+
+                entity.Property(e => e.DemandOrderId)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("DEMAND_ORDER_ID");
+
+                entity.Property(e => e.Dsd)
+                    .HasColumnType("DATE")
+                    .HasColumnName("DSD");
+
+                entity.Property(e => e.Ins)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("INS");
+
+                entity.Property(e => e.MfgOrderId)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("MFG_ORDER_ID");
+
+                entity.Property(e => e.Ncp)
+                    .HasMaxLength(2)
+                    .IsUnicode(false)
+                    .HasColumnName("NCP");
+
+                entity.Property(e => e.Npart)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("NPART");
+
+                entity.Property(e => e.Npech)
+                    .HasMaxLength(2)
+                    .IsUnicode(false)
+                    .HasColumnName("NPECH");
+
+                entity.Property(e => e.Nplav)
+                    .HasMaxLength(15)
+                    .IsUnicode(false)
+                    .HasColumnName("NPLAV");
+
+                entity.Property(e => e.OkonchPereplav)
+                    .HasMaxLength(1)
+                    .IsUnicode(false)
+                    .HasColumnName("OKONCH_PEREPLAV");
+
+                entity.Property(e => e.PasportId)
+                    .HasPrecision(10)
+                    .HasColumnName("PASPORT_ID");
+
+                entity.Property(e => e.Pereplav)
+                    .HasMaxLength(1)
+                    .IsUnicode(false)
+                    .HasColumnName("PEREPLAV");
+
+                entity.Property(e => e.Poz)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("POZ");
+
+                entity.Property(e => e.PozIl)
+                    .IsUnicode(false)
+                    .HasColumnName("POZ_IL");
+
+                entity.Property(e => e.PozNaim)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("POZ_NAIM");
+
+                entity.Property(e => e.PozRazm)
+                    .IsUnicode(false)
+                    .HasColumnName("POZ_RAZM");
+
+                entity.Property(e => e.RazmPasp)
+                    .IsUnicode(false)
+                    .HasColumnName("RAZM_PASP");
+
+                entity.Property(e => e.RazmSdch)
+                    .IsUnicode(false)
+                    .HasColumnName("RAZM_SDCH");
+
+                entity.Property(e => e.Splav)
+                    .HasMaxLength(80)
+                    .IsUnicode(false)
+                    .HasColumnName("SPLAV");
+
+                entity.Property(e => e.SumVesZapusk)
+                    .HasColumnType("NUMBER")
+                    .HasColumnName("SUM_VES_ZAPUSK");
+
+                entity.Property(e => e.Tek)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("TEK");
+
+                entity.Property(e => e.VesSdch)
+                    .HasColumnType("NUMBER(10,2)")
+                    .HasColumnName("VES_SDCH");
+
+                entity.Property(e => e.Zapusk31)
+                    .IsUnicode(false)
+                    .HasColumnName("ZAPUSK_31");
+
+                entity.Property(e => e.ZapuskNakl)
+                    .IsUnicode(false)
+                    .HasColumnName("ZAPUSK_NAKL");
+
+                entity.Property(e => e.ZapuskPpf)
+                    .HasMaxLength(44)
+                    .IsUnicode(false)
+                    .HasColumnName("ZAPUSK_PPF");
+            });
 
             modelBuilder.Entity<Melts31>(entity =>
             {
@@ -87,7 +212,6 @@ namespace Melts_Base.OracleModels
                     .HasColumnName("MELTERNUMBER");
 
                 entity.Property(e => e.Meltnumber)
-                    .IsRequired()
                     .HasMaxLength(50)
                     .IsUnicode(false)
                     .HasColumnName("MELTNUMBER");
