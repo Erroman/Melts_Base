ae8b490bddb6c7f7a9d6f0c2d5e94addefa32936
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-05-30 08:39:42 +0500

    Added context for Mappedmelt POCO class.

---
+++ Melts_Base/SQLiteModels/MappedmeltContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Melts_Base.SQLiteModels
+{
+    class MappedmeltContext : DbContext
+    {
+        public DbSet<Mappedmelt> Melts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=melts.db");
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
