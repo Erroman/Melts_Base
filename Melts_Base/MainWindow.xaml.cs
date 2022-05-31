ff5a5de0cc5e6a58e102995eced3c7c4e50a27c2
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-05-31 11:35:28 +0500

    Added button 'Export to Excel'.

--- Melts_Base/MainWindow.xaml.cs
+++ Melts_Base/MainWindow.xaml.cs
@@ -157,5 +157,33 @@ namespace Melts_Base
             }
             meltsContext.SaveChanges();
         }
+
+        private void ExportToExcel(object sender, RoutedEventArgs e)
+        {
+            
+                //Excel.Application excel = new Excel.Application();
+                //excel.Visible = true;
+                //Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+                //Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+
+                //for (int j = 0; j < DataGridParam.Columns.Count; j++)
+                //{
+                //    Range myRange = (Range)sheet1.Cells[1, j + 1];
+                //    sheet1.Cells[1, j + 1].Font.Bold = true;
+                //    sheet1.Columns[j + 1].ColumnWidth = 15;
+                //    myRange.Value2 = DataGridParam.Columns[j].Header;
+                //}
+                //for (int i = 0; i < DataGridParam.Columns.Count; i++)
+                //{
+                //    for (int j = 0; j < DataGridParam.Items.Count; j++)
+                //    {
+                //        TextBlock b = DataGridParam.Columns[i].GetCellContent(DataGridParam.Items[j]) as TextBlock;
+                //        Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
+                //        myRange.Value2 = b.Text;
+                //    }
+            
+                //}
+
+        }
     }
 }
