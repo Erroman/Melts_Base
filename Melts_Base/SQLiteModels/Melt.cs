286012df77a65a3663c6aa822876aed1231dbc34
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-06-30 10:02:55 +0500

    Removed redundant fileds from Melt of SQLite.

--- Melts_Base/SQLiteModels/Melt.cs
+++ Melts_Base/SQLiteModels/Melt.cs
@@ -10,22 +10,6 @@ namespace Melts_Base.SQLiteModels
     public class Melt
     {
         public int MeltId { get; set; }
-        public string MeltNumber { get; set; }
-        public DateTime? MeltDate { get; set; }
-        public string AlloyName { get; set; }
-        public string AlloyIndex { get; set; }
-        public short? MouldSet { get; set; }  //не найден
-        public short? ElectrodeDiameter { get; set; } //не найден
-        public string MelterNumber { get; set; } //не найден
-        public string MelterName { get; set; }   //не найден
-        public string TEKNumber { get; set; }   
-        public string IL_UiS_SHN { get; set; }
-        public string Contract { get; set; }
-        public string Supplement { get; set; }
-        public string Purpose { get; set; }
-        public short? IngotDiameter { get; set; }
-
-
         public string Npech { get; set; }
         public string Nplav { get; set; }
         public string Npart { get; set; }
