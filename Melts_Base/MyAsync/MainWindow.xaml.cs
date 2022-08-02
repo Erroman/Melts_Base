4a5874955f43abd37f15c06052ac7262b1976b76
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-08-02 16:38:39 +0500

    Added project MySync

---
+++ Melts_Base/MyAsync/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyAsync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void  Button_Click(object sender, RoutedEventArgs e)
+        {
+            progressBar.Value = 50;
+            await Task.Run(()=> longTask());
+        }
+        void longTask() { Thread.Sleep(5000);progressBar.Value += 50; }
+    }
+}
