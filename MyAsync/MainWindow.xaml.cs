531195daee5001db0c0ec9f794a3686dc16abc68
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-08-11 15:30:03 +0500

    Data refreshing in the separate RibbonGroup, the message about refreshing goes on.

--- MyAsync/MainWindow.xaml.cs
+++ MyAsync/MainWindow.xaml.cs
@@ -51,5 +51,10 @@ namespace MyAsync
             );
             return await task;
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
