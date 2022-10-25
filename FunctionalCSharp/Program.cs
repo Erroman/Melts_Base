5989d126180693352e793a204b8de12e79f065d7
Vladimir Romanovsky <vladimir_roman@mail.ru>
2022-10-25 16:40:33 +0500

    The Sieve of Eratosthene investigated.

--- FunctionalCSharp/Program.cs
+++ FunctionalCSharp/Program.cs
@@ -1,34 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-using System.Reflection.Metadata.Ecma335;
-using static StringExt;
-using NUnit.Framework;
-using LaYumba.Functional;
-using static LaYumba.Functional.F;
-using static System.Console;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using PrimeT = System.UInt32;
+Console.WriteLine(new PrimesBird().ElementAt(1000000 - 1)); // zero based indexing...
 
-Option<string> _ = None;
-Option<string> john = Some("John");
-Option<string> ku = None;
-ku = Some("Хуй");
-WriteLine(ku);
-
-public static class StringExt
+class PrimesBird : IEnumerable<PrimeT>
 {
-    public static string ToSentenceCase(this string s)
-       => s == string.Empty
-          ? string.Empty
-          : char.ToUpperInvariant(s[0]) + s.ToLower()[1..];
-}
-class ListFormatter
-{
-    int counter;
-
-    string PrependCounter(string s) => $"{++counter}. {s}";
-
-    public List<string> Format(List<string> list)
-       => list
-          .Select(StringExt.ToSentenceCase)
-          .Select(PrependCounter)
-          .ToList();
+    private struct CIS<T>
+    {
+        public T v; public Func<CIS<T>> cont;
+        public CIS(T v, Func<CIS<T>> cont)
+        {
+            this.v = v; this.cont = cont;
+        }
+    }
+    private CIS<PrimeT> pmlts(PrimeT p)
+    {
+        Func<PrimeT, CIS<PrimeT>> fn = null;
+        fn = (c) => new CIS<PrimeT>(c, () => fn(c + p));
+        return fn(p * p);
+    }
+    private CIS<CIS<PrimeT>> allmlts(CIS<PrimeT> ps)
+    {
+        return new CIS<CIS<PrimeT>>(pmlts(ps.v), () => allmlts(ps.cont()));
+    }
+    private CIS<PrimeT> merge(CIS<PrimeT> xs, CIS<PrimeT> ys)
+    {
+        var x = xs.v; var y = ys.v;
+        if (x < y) return new CIS<PrimeT>(x, () => merge(xs.cont(), ys));
+        else if (y < x) return new CIS<PrimeT>(y, () => merge(xs, ys.cont()));
+        else return new CIS<PrimeT>(x, () => merge(xs.cont(), ys.cont()));
+    }
+    private CIS<PrimeT> cmpsts(CIS<CIS<PrimeT>> css)
+    {
+        return new CIS<PrimeT>(css.v.v, () => merge(css.v.cont(), cmpsts(css.cont())));
+    }
+    private CIS<PrimeT> minusat(PrimeT n, CIS<PrimeT> cs)
+    {
+        var nn = n; var ncs = cs;
+        for (; ; ++nn)
+        {
+            if (nn >= ncs.v) ncs = ncs.cont();
+            else return new CIS<PrimeT>(nn, () => minusat(++nn, ncs));
+        }
+    }
+    private CIS<PrimeT> prms()
+    {
+        return new CIS<PrimeT>(2, () => minusat(3, cmpsts(allmlts(prms()))));
+    }
+    public IEnumerator<PrimeT> GetEnumerator()
+    {
+        for (var ps = prms(); ; ps = ps.cont()) yield return ps.v;
+    }
+    IEnumerator IEnumerable.GetEnumerator() { return (IEnumerator)GetEnumerator(); }
 }
